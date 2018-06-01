52f463fcf05ac177e93f5fb2082ea9923eb99801
Anthony Ngu <anthonyngu@Anthonys-MacBook-Pro.local>
2018-06-01 00:11:29 -0600

    final

--- assets/giftastic.js
+++ assets/giftastic.js
@@ -2,31 +2,33 @@ var sports = ["Baseball", "Football", "Basketball"];
 var sportAddition = "";
 var sportName = "";
 $("#submit").on("click", function() {
-    // Capturing the value of the input entry
+
+    var animal = $(this).attr("data-animal");
+
     sportAddition = $("#sport-addition").val()
-    // Creating the new button element and attributes
+  
     var newButton = $("<button>");
     newButton.attr("type", "button");
     newButton.attr("data-name", sportAddition);
     newButton.attr("class", "sport-button btn-primary btn-secondary");
     
-    // Dropping the value to the beginning of the fruits array
+   
     sports.unshift(sportAddition);
-    // Add the fruitAddition inside the text of the new button
+
     newButton.text(sportAddition);
-    // Appending the new button
+  
     $("#sport-buttons").append(newButton);
     
-    // Debugging
+
     console.log(sportAddition);
     console.log(sports);
 })
 $("#sport-buttons").on("click", ".sport-button", function() {
-    // Capture the text of the element in the 
+  
     sportName = $(this).text()
-    // Declaring URL variable to be used in API call
+ 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    sportName + "&api_key=cxh7RvHzjpIPl7D4JXOQqqKfmg4vzPC9"
+    sportName + "&api_key=cxh7RvHzjpIPl7D4JXOQqqKfmg4vzPC9&limit=10"
     ;
     $.ajax ({
         url: queryURL,
@@ -42,7 +44,13 @@ $("#sport-buttons").on("click", ".sport-button", function() {
             
             gifContainer.prepend(gifImage);
             $("#gifs").prepend(gifContainer);
+
+
+            var p = $("<p>").text("Rating: " + result[i].rating);
         }
     })
     
 })
+
+
+   
